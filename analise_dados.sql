05741443da1411b3254ff48ba31bd1b21667a233
Pascoal Fernandes <pascoal01ti@hotmail.com>
2023-05-12 10:52:02 -0300

    Update analise_dados.sql

--- analise_dados.sql
+++ analise_dados.sql
@@ -1,6 +1,8 @@
  -- Categorias com maior número de produtos cadastrados--
 SELECT top 10
-[product_category_name] AS " Categoria de produtos ", COUNT(*) AS "Total Produtos" FROM [dbo].[TB_OLIST_PRODUCTS]
+[product_category_name] AS " Categoria de produtos ",
+COUNT(*) AS "Total Produtos"
+FROM [dbo].[TB_OLIST_PRODUCTS]
 GROUP BY product_category_name
 ORDER BY COUNT(*) DESC
 
